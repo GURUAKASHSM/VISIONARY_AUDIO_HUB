f6a3baf514807de3fb84a369ec379463798fb8fe
GURUAKASH SM <guruakashsm@gmail.com>
2024-03-24 03:57:36 +0530

    updated pdf api & UI changes

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from PIL import Image
 from googletrans import Translator
 from gtts import gTTS
 import textwrap
+from PyPDF2 import PdfReader
 import PIL.Image
 import google.generativeai as genai
 
@@ -14,7 +15,7 @@ app = Flask(__name__)
 tess.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract'
 
 # Configure Google API key
-GOOGLE_API_KEY = "<--API-->"
+GOOGLE_API_KEY = "AIzaSyBfq78sE-iZ8RsT_kfP93nGi51UyoiVwH8"
 genai.configure(api_key=GOOGLE_API_KEY)
 
 # Function to display markdown
@@ -29,14 +30,27 @@ model = genai.GenerativeModel('gemini-pro-vision')
 if not os.path.exists('uploads'):
     os.makedirs('uploads')
 
+def extract_text_from_pdf(pdf_file):
+    try:
+        pdf_reader = PdfReader(pdf_file)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        return text
+    except Exception as e:
+        print("Error extracting text from PDF:", e)
+        return None
+
 @app.route('/')
 def index():
     return render_template('/home/index.html')
 
 @app.route('/process', methods=['POST'])
 def process():
+    print(request.files.keys())
     if 'user_text' in request.form and request.form['user_text'].strip() != '':
         # Handle text input
+        print("In TEXT File")
         user_text = request.form['user_text']
         selected_language = request.form.get('language')
 
@@ -58,7 +72,9 @@ def process():
 
         return render_template('/result/index.html', text_tesseract=translated_text, selected_language = selected_language, audio_path_tesseract=audio_path)
     
-    elif 'image' in request.files:
+    if 'image' in request.files:
+
+        print("In IMAGE File")
         # Handle image input
         image_file = request.files['image']
 
@@ -76,7 +92,12 @@ def process():
 
         # Generate content from image using GenerativeModel
         response = model.generate_content(img)
-        generated_text = response.text
+
+        # Check if response contains a valid Part
+        if hasattr(response, 'text'):
+            generated_text = response.text
+        else:
+            generated_text = "Content generation failed."
 
         # Determine the selected language
         selected_language = request.form.get('language')
@@ -114,11 +135,56 @@ def process():
                                selected_language=selected_language, 
                                audio_path_gemini=audio_path_gemini, 
                                audio_path_tesseract=audio_path_tesseract)
+    
+    if 'pdf' in request.files:
+            print("In PDF File")
+            pdf_file = request.files['pdf']
+            if pdf_file.filename == '':
+                return "No PDF file selected"
 
+            # Create the 'uploads' directory if it doesn't exist
+            if not os.path.exists('uploads'):
+                os.makedirs('uploads')
+            print(pdf_file.filename)
+            pdf_path = os.path.join('uploads', pdf_file.filename)
+            pdf_file.save(pdf_path)
+            
+            with open(pdf_path, 'rb') as file:
+                extracted_text = extract_text_from_pdf(file)
+
+            os.remove(pdf_path)
+
+            user_text = extracted_text
+            selected_language = request.form.get('language')
+
+            # Translate the user-provided text to the selected language
+            translator = Translator()
+            translated_text = translator.translate(user_text, src='en', dest=selected_language).text
+            print("trans--",user_text)
+            # Initialize the text-to-speech engine for the selected language
+            if selected_language == 'ta':
+                tts = gTTS(text=translated_text, lang='ta')
+            elif selected_language == 'hi':
+                tts = gTTS(text=translated_text, lang='hi')
+            else:
+                tts = gTTS(text=translated_text, lang='en')
+
+            # Save the audio file
+            audio_path = 'uploads/audio_speech.mp3'
+            tts.save(audio_path)
+
+            return render_template('/result/index.html', text_tesseract=translated_text, selected_language = selected_language, audio_path_tesseract=audio_path)
+        
+    else:
+        print("In No Data")
+        return "No valid input provided"
+   
+   
 # Serve static files over HTTPS
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory('uploads', filename)
+    uploads_dir = 'uploads'
+    return send_from_directory(uploads_dir, filename)
 
 if __name__ == '__main__':
     # Run the Flask app with HTTPS support
